d8e75ef3df82976447e4a3be0a54b79f89bb27a2
ProgrammerShahJalal <you@example.com>
2022-11-29 20:09:29 +0600

    solved

--- problem1.c
+++ problem1.c
@@ -2,17 +2,38 @@
 // Now print the total count of strictly less and strictly greater value from k. Implement it using function.
 // Note – In the given input, there may exist duplicate value.
 
-// Sample Input : 
+// Sample Input :
 // 7
 // 1 2 5 5 9 2 3 5
 
 // Sample Output :
 // 5
 
-#include<stdio.h>
+#include <stdio.h>
 
+int findOthers(int x, int ara[100]);
 int main()
 {
-    
+    int n;
+    int ara[100];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ara[i]);
+    }
+    findOthers(n, ara);
     return 0;
+}
+
+int findOthers(int x, int ara[100])
+{
+    int k, count = 0;
+
+    scanf("%d", &k);
+    for (int i = 0; i < x; i++)
+    {
+        if (ara[i] != k)
+            count++;
+    }
+    printf("%d\n", count);
 }
