b93c0294325b7d958aa178106c461ecfdc70279f
ProgrammerShahJalal <you@example.com>
2022-11-29 19:47:15 +0600

    first commit

---
+++ problem1.c
@@ -0,0 +1,18 @@
+// You are given a positive integer n. The second line will contain n positive integers. The third line will contain a value k.
+// Now print the total count of strictly less and strictly greater value from k. Implement it using function.
+// Note – In the given input, there may exist duplicate value.
+
+// Sample Input : 
+// 7
+// 1 2 5 5 9 2 3 5
+
+// Sample Output :
+// 5
+
+#include<stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
